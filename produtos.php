926eaf4b78f2a3f2db72dad8e8a437b8f4efce42
sidmaroli <119603972+sidmaroli@users.noreply.github.com>
2023-02-24 11:06:29 -0300

    Teste com twig

--- produtos.php
+++ produtos.php
@@ -7,6 +7,28 @@
 
     $template = $twig->load('produtos.html');
 
+
+    $produtos = [
+        [
+            'nome' => 'Chinelo',
+            'preco' => 30,
+        ],
+        [
+            'nome' => 'Camiseta',
+            'preco' => 50,
+        ],
+        [
+            'nome' => 'Boné',
+            'preco' => 39.9,
+        ],
+        [
+            'nome' => 'Automovéis simples',
+            'preco' => 3000.2,
+        ]
+    ];
+
     echo $template->render([
-        'titulo' => 'Produtos'
+        'titulo' => 'Produtos',
+        //'produtos' => $produtos,
+        'produtos' => null,
     ]);
