188ea2b3d570f559ffc0d2ca45af326d7567fe11
sidmaroli <119603972+sidmaroli@users.noreply.github.com>
2023-03-23 10:04:31 -0300

    js

---
+++ js/olho.js
@@ -0,0 +1,6 @@
+const olho = document.getElementById('olho');
+const campo = document.querySelector('#senha');
+
+olho.addEventListener('click', () => {
+    campo.type = campo.type == 'password' ? 'text' : 'password';
+});
--- recuperar_senha_escolher.php
+++ recuperar_senha_escolher.php
@@ -2,18 +2,32 @@
 require('twig_carregar.php');
 require('pdo.inc.php');
 
-$token = $_GET['token'] ?? false;
+$token = $_GET['token'] ?? $_POST['token'] ?? false;
 
 if(!$token){
     header('location:login.php');
     die;
 }
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $password = $_POST['senha'] ?? false;
+    $password = trim($password);
+    //Tratamento para  o caso de senha falsa
 
+    $sql = $pdo->prepare('UPDATE usuarios SET senha = :senha, recupera_token = NULL WHERE recupera_token = :token');
+    $sql->execute([
+        ':senha' => password_hash($password, PASSWORD_BCRYPT),
+        ':token' => $token, 
+    ]);
+
+    header('location:login.php?erro=3');
+
+    die;
+}
 $sql = $pdo->prepare('SELECT * FROM usuarios WHERE recupera_token = ?');
 $sql->execute([$token]);
 
-if($sql->rowCount == 1){
-    $twig->render('recuperar_senha_escolher.html');
+if($sql->rowCount() == 1){
+   echo $twig->render('recuperar_senha_escolher.html', ['token' => $token]);
 }else{
     header('location:login.php');
     die;
